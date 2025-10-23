8870319597754e9c5ee946089ec4731038b8cd7a
ezzyjuliansyah <ezzyjuliansyah1@gmail.com>
2025-10-23 09:54:48 +0700

    menetukan komisi penjualan

--- KomisiPenjualan09.java
+++ KomisiPenjualan09.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class KomisiPenjualan01 {
+public class KomisiPenjualan09 {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
 
